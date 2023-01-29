91f902d2e7ef8e38119cfae0428aa0e2d7c304a5
Roh Jun Seok <63935813+RJ-Stony@users.noreply.github.com>
2023-01-29 14:52:05 +0900

    Update happiness_index.py

--- happiness_index.py
+++ happiness_index.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import pandas as pd
-import zipfile
+from zipfile import ZipFile
+from StringIO import StringIO
+from urllib import urlopen
 
 df = dict()
 
-zf = zipfile.ZipFile('https://drive.google.com/file/d/1R17iOJIqiOsjKeU-cHZtzuCnyRCBgkwJ/view?usp=share_link')
+resp = urlopen('https://drive.google.com/file/d/1R17iOJIqiOsjKeU-cHZtzuCnyRCBgkwJ/view?usp=share_link')
+zf = ZipFile(StringIO(resp.read()))
+
 df['2015'] = pd.read_csv(zf.open('2015.csv')); df['2016'] = pd.read_csv(zf.open('2016.csv'));
 df['2017'] = pd.read_csv(zf.open('2017.csv')); df['2018'] = pd.read_csv(zf.open('2018.csv'))
 df['2019'] = pd.read_csv(zf.open('2019.csv')); df['2020'] = pd.read_csv(zf.open('2020.csv'))
