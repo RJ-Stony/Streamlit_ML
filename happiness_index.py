e791bb06fef8077d1a287d1b4e38be3bc8c1f583
Roh Jun Seok <63935813+RJ-Stony@users.noreply.github.com>
2023-01-29 13:36:39 +0900

    Update happiness_index.py

--- happiness_index.py
+++ happiness_index.py
@@ -4,6 +4,6 @@ with st.sidebar:
     uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 
 for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
+    if uploaded_file is not None:
+        uploaded_df = pd.read_csv(uploaded_file)
+    st.write(uploaded_df)
