09f9974044ac247a3b30b3f3110f6d84f5f4730c
Roh Jun Seok <63935813+RJ-Stony@users.noreply.github.com>
2023-01-29 15:08:00 +0900

    Update happiness_index.py

--- happiness_index.py
+++ happiness_index.py
@@ -5,7 +5,7 @@ import streamlit.components.v1 as components
 
 df = dict()
 
-zf = ZipFile('https://github.com/RJ-Stony/Streamlit_ML/blob/main/archive.zip')
+zf = ZipFile('https://github.com/RJ-Stony/Streamlit_ML/blob/e3b30c59cd3f8e9c0909d34943d6dc394b141f0a/archive.zip?raw=true')
 df['2015'] = pd.read_csv(zf.open('2015.csv')); df['2016'] = pd.read_csv(zf.open('2016.csv'));
 df['2017'] = pd.read_csv(zf.open('2017.csv')); df['2018'] = pd.read_csv(zf.open('2018.csv'))
 df['2019'] = pd.read_csv(zf.open('2019.csv')); df['2020'] = pd.read_csv(zf.open('2020.csv'))
