e3b30c59cd3f8e9c0909d34943d6dc394b141f0a
Roh Jun Seok <63935813+RJ-Stony@users.noreply.github.com>
2023-01-29 15:05:25 +0900

    Update happiness_index.py

--- happiness_index.py
+++ happiness_index.py
@@ -5,31 +5,12 @@ import streamlit.components.v1 as components
 
 df = dict()
 
-with open('https://drive.google.com/file/d/1R17iOJIqiOsjKeU-cHZtzuCnyRCBgkwJ/view?usp=share_link', "rb") as fp:
-    btn = st.download_button(
-        label="Download ZIP",
-        data=fp,
-        file_name="myfile.zip",
-        mime='application/x-zip-compressed'
-    )
-
-with st.sidebar:
-    uploaded_files = st.file_uploader('CSV 파일을 업로드해주세요.', accept_multiple_files=True)
-# Check if files were uploaded
-if len(uploaded_files) > 0:
-    for uploaded_file in uploaded_files:
-        # If zip file, extract contents
-        if uploaded_file.type == 'application/x-zip-compressed':
-            zf = ZipFile(uploaded_file)
-            df['2015'] = pd.read_csv(zf.open('2015.csv')); df['2016'] = pd.read_csv(zf.open('2016.csv'));
-            df['2017'] = pd.read_csv(zf.open('2017.csv')); df['2018'] = pd.read_csv(zf.open('2018.csv'))
-            df['2019'] = pd.read_csv(zf.open('2019.csv')); df['2020'] = pd.read_csv(zf.open('2020.csv'))
-            df['2021'] = pd.read_csv(zf.open('2021.csv')); df['2022'] = pd.read_csv(zf.open('2022.csv'))
+zf = ZipFile('https://github.com/RJ-Stony/Streamlit_ML/blob/main/archive.zip')
+df['2015'] = pd.read_csv(zf.open('2015.csv')); df['2016'] = pd.read_csv(zf.open('2016.csv'));
+df['2017'] = pd.read_csv(zf.open('2017.csv')); df['2018'] = pd.read_csv(zf.open('2018.csv'))
+df['2019'] = pd.read_csv(zf.open('2019.csv')); df['2020'] = pd.read_csv(zf.open('2020.csv'))
+df['2021'] = pd.read_csv(zf.open('2021.csv')); df['2022'] = pd.read_csv(zf.open('2022.csv'))
             
-            st.write(df['2015'])
-            st.write(df['2022'])
-    
-        elif uploaded_file.type == 'text/csv':
-            uploaded_df = pd.read_csv(uploaded_file)
-            st.write(uploaded_df)
+st.write(df['2015'])
+st.write(df['2022'])
             
