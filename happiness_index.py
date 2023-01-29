b58e497806fee6fa2e57b5310eb7f9dbd3f06370
Roh Jun Seok <63935813+RJ-Stony@users.noreply.github.com>
2023-01-29 14:24:55 +0900

    Update happiness_index.py

--- happiness_index.py
+++ happiness_index.py
@@ -1,10 +1,15 @@
 import streamlit as st
 import pandas as pd
+import zipfile
 
 with st.sidebar:
     uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 
-for uploaded_file in uploaded_files:
-    if uploaded_file is not None:
-        uploaded_df = pd.read_csv(uploaded_file)
+with zipfile.ZipFile(uploaded_files, "r") as z:
+    z.extractall(".")
+    
+    if z is not None:
+        uploaded_df = pd.read_csv(z)
     st.write(uploaded_df)
+
+    
