8a466d359c914e20676016d32790e122aba9a893
Roh Jun Seok <63935813+RJ-Stony@users.noreply.github.com>
2023-01-29 15:17:57 +0900

    Update happiness_index.py
    Add activate_sidebar function

--- happiness_index.py
+++ happiness_index.py
@@ -5,23 +5,28 @@ import streamlit.components.v1 as components
 
 df = dict()
 
-with st.sidebar:
-    uploaded_files = st.file_uploader('CSV 파일 혹은 ZIP 파일을 업로드해주세요.', accept_multiple_files=True)
-# Check if files were uploaded
-if len(uploaded_files) > 0:
-    for uploaded_file in uploaded_files:
-        # If zip file, extract contents
-        if uploaded_file.type == 'application/x-zip-compressed':
-            zf = ZipFile(uploaded_file)
-            df['2015'] = pd.read_csv(zf.open('2015.csv')); df['2016'] = pd.read_csv(zf.open('2016.csv'));
-            df['2017'] = pd.read_csv(zf.open('2017.csv')); df['2018'] = pd.read_csv(zf.open('2018.csv'))
-            df['2019'] = pd.read_csv(zf.open('2019.csv')); df['2020'] = pd.read_csv(zf.open('2020.csv'))
-            df['2021'] = pd.read_csv(zf.open('2021.csv')); df['2022'] = pd.read_csv(zf.open('2022.csv'))
-            
-            st.write(df['2015'])
-            st.write(df['2022'])
-    
-        elif uploaded_file.type == 'text/csv':
-            uploaded_df = pd.read_csv(uploaded_file)
-            st.write(uploaded_df)
-            
+def activate_sidebar():
+    with st.sidebar:
+        uploaded_files = st.file_uploader('CSV 파일 혹은 ZIP 파일을 업로드해주세요.', accept_multiple_files=True)
+    # Check if files were uploaded
+    if len(uploaded_files) > 0:
+        for uploaded_file in uploaded_files:
+            # If zip file, extract contents
+            if uploaded_file.type == 'application/x-zip-compressed':
+                zf = ZipFile(uploaded_file)
+                df['2015'] = pd.read_csv(zf.open('2015.csv')); df['2016'] = pd.read_csv(zf.open('2016.csv'));
+                df['2017'] = pd.read_csv(zf.open('2017.csv')); df['2018'] = pd.read_csv(zf.open('2018.csv'))
+                df['2019'] = pd.read_csv(zf.open('2019.csv')); df['2020'] = pd.read_csv(zf.open('2020.csv'))
+                df['2021'] = pd.read_csv(zf.open('2021.csv')); df['2022'] = pd.read_csv(zf.open('2022.csv'))
+
+                st.write(df['2015'])
+                st.write(df['2022'])
+
+            elif uploaded_file.type == 'text/csv':
+                uploaded_df = pd.read_csv(uploaded_file)
+                st.write(uploaded_df)
+
+activate_sidebar()
+
+for key in df:
+  print(key, df[key].columns) # 각 데이터프레임의 컬럼 확인
