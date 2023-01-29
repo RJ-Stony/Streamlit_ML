6a274c8d18df94065e0c84b67a949cd9f2874292
Roh Jun Seok <63935813+RJ-Stony@users.noreply.github.com>
2023-01-29 13:34:38 +0900

    Update happiness_index.py

--- happiness_index.py
+++ happiness_index.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 with st.sidebar:
     uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-    for uploaded_file in uploaded_files:
-        bytes_data = uploaded_file.read()
-        st.write("filename:", uploaded_file.name)
-        st.write(bytes_data)
+
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
